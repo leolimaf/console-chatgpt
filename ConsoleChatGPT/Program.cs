a4ff0ac1c6966cb2a470d0319e151111a2f55e94
Leonardo Lima <leonardolimaf54@gmail.com>
2023-06-09 02:53:18 -0300

    Adicionado app console p/ testar a API do chatGPT

---
+++ ConsoleChatGPT/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+
+do
+{
+    var input = Console.ReadLine();
+
+    if (input?.Length <= 0)
+        Console.WriteLine("Entrada inválida!\n");
+    else
+    {
+        var client = new HttpClient();
+        client.DefaultRequestHeaders.Add("authorization",
+            "Bearer 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");
+
+        var body = JsonConvert.SerializeObject(new
+        {
+            model = "text-davinci-003",
+            prompt = input,
+            max_tokens = 500,
+            temperature = 0
+        });
+
+        var httpResponse = await client.PostAsync("https://api.openai.com/v1/completions",
+            new StringContent(body, Encoding.UTF8, "application/json"));
+
+        var data = await httpResponse.Content.ReadAsStringAsync();
+
+        var response = JsonConvert.DeserializeObject<dynamic>(data);
+        Console.WriteLine(response?.choices[0].text);
+        Console.WriteLine();
+    }
+} while (true);
