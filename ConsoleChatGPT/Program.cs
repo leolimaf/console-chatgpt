f17501bab489748409359ed7808f3542105f6661
Leonardo Lima <leolimaf@outlook.com>
2024-02-28 16:00:56 -0300

    testes utilizando o model ChatGPTTurbo

--- ConsoleChatGPT/Program.cs
+++ ConsoleChatGPT/Program.cs
@@ -6,37 +6,42 @@ do
 {
     var input = Console.ReadLine();
 
-    if (input?.Length <= 0)
-        Console.WriteLine("Entrada inválida!\n");
-    else
+    if (string.IsNullOrWhiteSpace(input))
+        break;
+    
+    var client = new HttpClient();
+    client.DefaultRequestHeaders.Add("authorization",
+        "Bearer sk-fgsgsgssrgvrsgsg");
+
+    var body = JsonConvert.SerializeObject(new
     {
-        var client = new HttpClient();
-        client.DefaultRequestHeaders.Add("authorization",
-            "Bearer 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");
-
-        var body = JsonConvert.SerializeObject(new
+        model = "gpt-3.5-turbo",
+        messages =  new []
         {
-            model = "text-davinci-003",
-            prompt = input,
-            max_tokens = 500,
-            temperature = 0
-        });
-
-        try
-        {
-            var httpResponse = await client.PostAsync("https://api.openai.com/v1/completions",
-                new StringContent(body, Encoding.UTF8, "application/json"));
-
-            var data = await httpResponse.Content.ReadAsStringAsync();
-
-            var response = JsonConvert.DeserializeObject<dynamic>(data);
-            Console.WriteLine(response?.choices[0].text);
-            Console.WriteLine();
+            new {
+                role = "system",
+                content = "You are an expert in software development, skilled at explaining technology concepts used by Microsoft."
+            },
+            new {
+                role = "user",
+                content = input
+            }
         }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-            break;
-        }
+    });
+
+    try
+    {
+        var httpResponse = await client.PostAsync("https://api.openai.com/v1/chat/completions",
+            new StringContent(body, Encoding.UTF8, "application/json"));
+
+        var data = await httpResponse.Content.ReadAsStringAsync();
+
+        var response = JsonConvert.DeserializeObject<dynamic>(data);
+        Console.WriteLine(response?.choices[0].message.content);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+        break;
     }
 } while (true);
